8538f241540680d9001186dd00074c4560b64102
Andoni Arostegui <andoni.arostegui@stacknvault.com>
2021-01-11 15:16:23 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,37 @@ const parseYAML = async () => {
     console.log(mustache.render('{{host.name.value}}', config));
 }
 
+// const object1 = {
+//     name: 'Flavio',
+//     foo: {
+//         bar: 'hey',
+//         baz: 'meh',
+//     }
+//   }
+  
+//   const object2 = {
+//     age: 35,
+//     foo: {
+//         bar: 'hooo',
+//         pla: 'mik',
+//     }
+//   }
+  
+//   // Merge a `source` object to a `target` recursively
+//   const merge = (target, source) => {
+//     // Iterate through `source` properties and if an `Object` set property to merge of `target` and `source` properties
+//     for (const key of Object.keys(source)) {
+//       if (source[key] instanceof Object) Object.assign(source[key], merge(target[key], source[key]))
+//     }
+  
+//     // Join `target` and modified `source`
+//     Object.assign(target || {}, source)
+//     return target
+//   }
+  
+//   const object3 = merge(object1, object2);
+  
+  
+
 parseJSON();
 parseYAML();
