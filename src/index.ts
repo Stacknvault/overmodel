78a4562a3617b5e0fd9d8934f1f900375d35daa1
Andoni Arostegui <andoni.arostegui@stacknvault.com>
2021-01-12 22:00:56 +0100

    Model parsing complete

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 // import fs from 'fs';
-// import YAML from 'yaml';
+import YAML from 'yaml';
+
+import { readFileSync, readdirSync, fstat } from "fs";
+import properties from 'dot-properties';
 
 // const parseJSON = async () => {
 //     const rawData = fs.readFileSync('test/config/model/test.json', 'utf8');
@@ -33,17 +36,61 @@
 //     }
 //   }
   
-//   // Merge a `source` object to a `target` recursively
-//   const merge = (target, source) => {
+// // Merge a `source` object to a `target` recursively
+// const merge = (target, source) => {
 //     // Iterate through `source` properties and if an `Object` set property to merge of `target` and `source` properties
 //     for (const key of Object.keys(source)) {
-//       if (source[key] instanceof Object) Object.assign(source[key], merge(target[key], source[key]))
+//         if (source[key] instanceof Object) Object.assign(source[key], merge(target[key], source[key]))
 //     }
-  
+
 //     // Join `target` and modified `source`
 //     Object.assign(target || {}, source)
 //     return target
-//   }
+// }
+let merge = (...objects: {}[]) => {
+    let target = {};
+    // Merge the object into the target object
+    let merger = (obj) => {
+        for (let prop in obj) {
+          if (obj.hasOwnProperty(prop)) {
+            if (Object.prototype.toString.call(obj[prop]) === '[object Object]'){
+              // If we're doing a deep merge 
+              // and the property is an object
+              target[prop] = merge(target[prop], obj[prop]);
+            } else {
+              // Otherwise, do a regular merge
+              target[prop] = obj[prop];
+            }
+           }
+        }
+    };
+     //Loop through each object and conduct a merge
+     for (let i = 0; i < objects.length; i++) {
+        merger(objects[i]);
+     }
+       return target;
+  };
+const getOject = (str) => {
+    // this turns the string into an array = 'one.two.three' becomes ['one', 'two', 'three']
+    var arr = str.split('.');
+
+    // this will be our final object
+    var obj = {};
+
+    // this is the current level of the object - in the first iteration we will add the "one" object here
+    var curobj = obj;
+
+    var i = 0;
+    // we loop until the next-to-last element because we want the last element ("three") to contain an empty string instead of an empty object
+    while (i < (arr.length-1)) {
+        // add a new level to the object and set the curobj to the new level
+        curobj[arr[i]] = {};
+        curobj = curobj[arr[i++]];
+    }
+    // finally, we append the empty string to the final object
+    curobj[arr[i]] = '';
+    return obj;
+}
   
 //   const object3 = merge(object1, object2);
   
@@ -52,4 +99,119 @@
 // parseJSON();
 // parseYAML();
 
-console.log('Just a hello world');
+const validCommands = ['apply'];
+const validConfigExtensions = ['json', 'yaml', 'properties'];
+const isCommandValid = (command) => validCommands.filter(c=>c===command).length===1;
+var walk    = require('walk');
+
+interface Arguments {
+    model: string[];
+    rule: string[];
+    _: string[];
+}
+interface ModelFile {
+    root: string;
+    file: string;
+}
+const walkDir = async (path: string) => {
+    return new Promise((resolve, reject)=>{
+        var files:string[] = [];
+        const walker = walk.walk(path, { followLinks: true });
+        walker.on('file', (root, stat, next) => {
+            // Add this file to the list of files
+            files.push(`${root}/${stat.name}`);
+            next();
+        });
+        walker.on('end', function() {
+           resolve(files);
+        });
+    });   
+}
+const parseProperties = (contents: string) => {
+    const parsedFile=properties.parse(contents);
+    var result={}
+    Object.keys(parsedFile).map(key=>{
+        const newKey = key + (key.indexOf('_metadata')<0?'.value':'');
+        console.log(`${key}, ${newKey}`);
+        var obj = getOject(newKey);
+        eval(`obj.${newKey}=\"${parsedFile[key]}\"`)
+        console.log('object gotten', JSON.stringify(obj));
+        console.log('merging with', JSON.stringify(result));
+        result=merge(result, obj);
+    });
+    return result;
+}
+const apply = async (args: Arguments) => {
+    console.log('Applying configuration');
+    const modelDirs = args['model-dir'];
+    const rules = args.rule||[];
+    if (!modelDirs || modelDirs.length === 0){
+        console.error('No model directories given');
+        return;
+    }
+    const files = (await Promise.all(modelDirs.map(async (modelDir: string)=>{
+        const files = await walkDir(modelDir) as string[];
+        return files.map((file: string)=>({root: modelDir, file: file.substring(modelDir.length+1)} as ModelFile));
+    }))).flatMap(item=>item)
+    .sort((item1, item2) => {
+        const file1 = item1 as ModelFile;
+        const file2 = item2 as ModelFile;
+        const depth1 = file1.file.split('/').length -1;
+        const depth2 = file2.file.split('/').length -1;
+        if (depth1 !== depth2){
+            return depth1 - depth2
+        }
+        return file1.file.localeCompare((item2 as ModelFile).file)
+    });
+    console.log(files);
+    const configuration = files
+    .filter(item=>{
+        // we see if the rules apply
+        var remainingFile=(item as ModelFile).file;
+        if (remainingFile.indexOf('/')<0){
+            return true;
+        }
+        console.log('The remaining file was', remainingFile);
+        rules.map((rule:string)=>{
+            remainingFile=remainingFile.replace(rule.replace(/=/gi, '/'), '');
+        });
+        const matches = remainingFile.startsWith('.') && validConfigExtensions.filter(item=>item===remainingFile.substring(1)).length>0;
+        console.log('The remaining file is', remainingFile, matches);
+        return matches;
+    })
+    .map((file)=>{
+        const modelFile=file as ModelFile;
+        return (modelFile.file.endsWith('json') && JSON.parse(readFileSync(`${modelFile.root}/${modelFile.file}`, 'utf-8')))
+            || (modelFile.file.endsWith('yaml') && YAML.parse(readFileSync(`${modelFile.root}/${modelFile.file}`, 'utf-8')))
+            || (modelFile.file.endsWith('properties') && parseProperties(readFileSync(`${modelFile.root}/${modelFile.file}`, 'utf-8')))
+            || {};
+    })
+    .reduce((previous, current)=>merge(previous, current));
+    console.log(JSON.stringify(configuration, null, 2));
+}
+const ubermodel = () => {
+    const usage = () =>{
+        console.log('This is how you use this:')
+    }
+    
+    var args = require('minimist')(process.argv.slice(2)) as Arguments;
+    if (args._.length===0){
+      console.error('Command is missing');
+      usage();
+      return;
+    }
+    const command=args._[0];
+    // const _=args._;
+    // args = _;
+    
+    console.log('args', args);
+
+    if (!isCommandValid(command)){
+        console.error('Invalid command');
+        usage();
+        return;
+    }
+    eval(command)(args);
+}
+// console.log(JSON.stringify(parseProperties(readFileSync('../poc-config-model/model/environment/staging.properties', 'utf-8')), null, 2))
+ubermodel();
